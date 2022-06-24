0967f50bf6e7148388b1485fed0f5b1e1b7f418c
Beau Elliott <belliott1516@gmail.com>
2022-06-24 16:11:58 -0700

    added new test to check for login and redirect

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -17,13 +17,20 @@ describe('backend-express-template routes', () => {
     );
   });
 
-  // it('/ should redirect to the gihub OAuth login page', async () => {
-  //   const res = await request(app).get('/api/v1/github/login');
+  it('/ should login and redirect users to /api/v1/github/dashboard', async () => {
+    const res = await request.agent(app)
+      .get('/api/vq/github/callback?code=42')
+      .redirects(1);
 
-  //   expect(res.header.location).toMatch(
-  //     /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
-  //   );
-  // });
+    expect(res.body).toEqual({
+      id: expect.any(String), 
+      login: 'Billy Bucherson', 
+      avatar_url: 'https://www.placecage.com/gif/200/200',
+      email: 'billy@bucherson.gov',
+      iat: expect.any(Number), 
+      exp: expect.any(Number),
+    });
+  });
 
   afterAll(() => {
     pool.end();
