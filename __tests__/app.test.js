064329f6659f650a69d82b426d1304607907b8fe
Beau Elliott <belliott1516@gmail.com>
2022-06-24 17:43:13 -0700

    got second and third tests to pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,7 +9,7 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('/ should redirect to the gihub OAuth login page', async () => {
+  it('/ should redirect to the github OAuth login page', async () => {
     const res = await request(app).get('/api/v1/github/login');
 
     expect(res.header.location).toMatch(
@@ -19,17 +19,24 @@ describe('backend-express-template routes', () => {
 
   it('/ should login and redirect users to /api/v1/github/dashboard', async () => {
     const res = await request.agent(app)
-      .get('/api/vq/github/callback')
+      .get('/api/v1/github/callback?code=42')
       .redirects(1);
 
     expect(res.body).toEqual({
       id: expect.any(String), 
-      login: 'Billy Bucherson', 
+      username: 'Billy Bucherson', 
       avatar_url: 'https://www.placecage.com/gif/200/200',
       email: 'billy@bucherson.gov',
       iat: expect.any(Number), 
       exp: expect.any(Number),
     });
+  });
+
+  it('/sessions should remove the cookie and sign out the user', async () => {
+    const res = await request.agent(app)
+      .delete('/api/v1/github/sessions');
+
+    expect(res.body).toEqual({ success: true, message: 'You have successfully signed out' });
   });
 
   afterAll(() => {
